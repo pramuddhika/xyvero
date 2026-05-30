600c4fef6db8f73a09e4137cfdd2bdef2235de56
pramuddhika <pramuddhika.rasanga@gmail.com>
2026-05-30 09:49:56 +0530

    Branding: use Xyvero logo, update title and colors, remove template assets

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,14 @@
 import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import logo from './assets/logo.jpeg'
 
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
+      <img alt="Xyvero logo" className="logo" src={logo} />
+      <div className="creator">Xyvero</div>
+      <div className="text">Xyvero</div>
       <p className="tip">
         Please try pressing <code>F12</code> to open the devTool
       </p>
