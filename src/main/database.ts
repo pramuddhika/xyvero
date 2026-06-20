e7afc7a2f2e786de31f6159f750aefaa2ce3b482
pramuddhika <pramuddhika.rasanga@gmail.com>
2026-06-20 14:20:30 +0530

    feat: add accountTypes and categoryTypes tables with initial data to database

--- src/main/database.ts
+++ src/main/database.ts
@@ -24,9 +24,19 @@ function createTables(database: Database.Database): void {
       configuration_id INTEGER PRIMARY KEY,
       configuration_key VARCHAR(200) NOT NULL UNIQUE,
       configuration_value VARCHAR(100) NOT NULL
-    )
+    );
+    CREATE TABLE IF NOT EXISTS accountTypes (
+      account_type_id INTEGER PRIMARY KEY,
+      account_type VARCHAR(100) NOT NULL,
+      account_type_name VARCHAR(100) NOT NULL UNIQUE
+    );
+    CREATE TABLE IF NOT EXISTS categoryTypes (
+      category_id INTEGER PRIMARY KEY,
+      category_type VARCHAR(100) NOT NULL,
+      category_name VARCHAR(100) NOT NULL UNIQUE
+    );
   `)
-
+ 
   database.exec(`
     INSERT OR IGNORE INTO configuration (configuration_id, configuration_key, configuration_value)
     VALUES
@@ -34,7 +44,22 @@ function createTables(database: Database.Database): void {
       (2, 'MONTH_START_DATE', '1'),
       (3, 'WEEK_START_DATE', 'Monday'),
       (4, 'THEME', 'dark'),
-      (5, 'FIRST_VIEW', 'Calendar')
+      (5, 'FIRST_VIEW', 'Calendar');
+
+    INSERT OR IGNORE INTO accountTypes (account_type_id, account_type, account_type_name)
+    VALUES
+      (1, 'CASH', 'Cash'),
+      (2, 'ACCOUNT', 'Accounts'),
+      (3, 'SAVING', 'Savings'),
+      (4, 'INVESTMENT', 'Investments'),
+      (5, 'LOAN', 'Loans'),
+      (6, 'CARD', 'Cards'),
+      (7, 'OTHER', 'Other');
+
+    INSERT OR IGNORE INTO categoryTypes (category_id, category_type, category_name)
+    VALUES
+      (1, 'IN', 'In'),
+      (2, 'OUT', 'Out');
   `)
 }
 
